8912e0216492b5efc13328cb74bce95d0896cee4
guywithoutnamenumber1-a11y <guywithoutnamenumber1@gmail.com>
2026-02-22 15:42:20 +0000

    Update chunk.js

--- js/chunk.js
+++ js/chunk.js
@@ -23,7 +23,7 @@ function loadTex(path) {
 
 const BLOCK_TEXTURES = {
 	[BLOCKS.GRASS]: {
-		top: loadTex('https://i.imgur.com/mIfJHmt.png'),
+		top: loadTex('https://i.imgur.com/9PeECGN.png'),
 		side: loadTex('https://i.imgur.com/2uDKrIn.png'),
 		bottom: loadTex('https://i.imgur.com/2uDKrIn.png')
 	},
